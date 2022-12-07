4c136fdcec56a7f1bc55840e1aeec85209ed03d5
Augusto Castejon <75548446+Augusto-Castejon@users.noreply.github.com>
2022-12-07 15:45:44 -0300

    Learning.002
    Fundo, titulo e tabela

--- script.js
+++ script.js
@@ -8,40 +8,37 @@ function ex1(){
 }
 
 function ex2(){
-    x = prompt("Digite um nome:");
+    x = prompt("Digite algum nome ou frase e descubra quantas letras tem:");
     alert("O nome tem " + x.length + " letras.");
 }
 
 function ex3(){
-    x = prompt("Digite a senha:");
-    if(x==='M4R1A')
-        alert("A senha está correta");
+    x = prompt("Qual o nome do menino que foi preso em uma casa de doces:");
+    if(x==='João')
+        alert("Correto!");
     else
-        alert("A senha está incorreta");
+        alert("Incorreto!");
 }
 
 function ex4(){
+    let y = prompt("Digite um número de 1 a 5, cada um mostrará uma ciade:");
     const city = ["Goiania", "Rio de Janeiro", "Belo Horizonte", "Uberaba", "São Paulo"];
-    alert(city[3]);
+    alert(city[y]);
 }
 
 function ex5(){
     const frutas = ["Banana", " Laranja", " Maçã", " Manga"];
-    frutas.push(" Abacaxi");
+    y = prompt("Digite uma fruta:");
+    frutas.push(" y");
     alert(frutas);
 }
 
 function ex6(){
-    x = prompt("Digite um nome:");
-    alert("O nome tem " + x.length + " letras.");
-}
-
-function ex7(){
-    x = prompt("Digite o nome de uma cidade:");
+    x = prompt("Digite algo e veja crescer:");
     alert(x.toUpperCase());
 }
 
-function ex8(){
+function ex7(){
     alert("Você está no mês " + (new Date().getMonth()+1))
 }
 
@@ -51,7 +48,16 @@ function ex9(){
     alert("Hoje é " + (dia[i]));
 }
 
-function ex10(){
-    i = new Date().getDate();
-    alert("Daqui a 60 dias será o dia " + (i+60));
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+
+  });
 }
+const titulo = document.querySelector('.titulo-principal');
+typeWrite(titulo);
